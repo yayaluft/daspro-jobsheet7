ce375af2d4fb29dc4979ef91c1a78e884d39f206
yayaluft <syanuha66@gmail.com>
2025-10-24 13:02:26 +0700

    Percobaan 2 modifikasi

--- SiakadWhile11.java
+++ SiakadWhile11.java
@@ -3,19 +3,20 @@ public class SiakadWhile11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int nilai, jml, i=0;
-        System.out.println("Masukkan jumlah mahasiswa: ");
+        System.out.print("Masukkan jumlah mahasiswa: ");
         jml = sc.nextInt();
 
-        while (i < jml) {
-            System.out.println("Masukkan nilai mahasiswa ke-"+ (i+1)+ ": ");
+        while (i < jml) { 
+            System.out.print("Masukkan nilai mahasiswa ke-"+ (i+1)+ ": ");
             nilai = sc.nextInt();
-
+            
             if (nilai < 0 || nilai > 100) {
                 System.out.println("Nilai tidak valid. Masukkan lagi nilai yang valid!");
                 continue;
             }
             if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai mahasiswa ke-"+ (i+1)+" adalah A");
+                System.out.println("Bagus, pertahankan nilainya");
             } else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai mahasiswa ke-" +(i+1)+ " adalah B+");
             } else if (nilai > 65 && nilai <= 73) {
